3fba9f541a70bb0726570dae180e5178349e25a4
joe <nagoromjoel8@gmail.com>
2026-09-24 07:16:22 +0100

    Support branch-based GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS
-    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
-    : "/",
+  base:
+    process.env.VITE_BASE_PATH ||
+    (process.env.GITHUB_ACTIONS
+      ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+      : "/"),
 })
